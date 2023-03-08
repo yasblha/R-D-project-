5eaa148072f954fa8d38669c5c325eaf186cefbf
yasblha <108906340+yasblha@users.noreply.github.com>
2023-03-08 11:45:53 +0100

    footer XXXX

--- js/slider.js
+++ js/slider.js
@@ -1,31 +1,31 @@
-  function afficherElements(dataAttr) {
-  // Sélectionner tous les éléments avec l'attribut de données spécifié
-  const elements = document.querySelectorAll(`[data-type="${dataAttr}"]`);
+/* function afficherElements(dataAttr) {
+    // Sélectionner tous les éléments avec l'attribut de données spécifié
+    const elements = document.querySelectorAll(`[data-type="${dataAttr}"]`);
   
-  // Ajouter la classe "block" à chaque élément sélectionné
+    // Ajouter la classe "block" à chaque élément sélectionné
+    elements.forEach(element => {
+      element.classList.add('block');
+    });
+  } */
+
+
+  function afficherElements(dataAttr) {  
+  const elements = document.querySelectorAll(`[data-type="${dataAttr}"]`); 
+  
   elements.forEach(element => {
     element.classList.add('block');
-  });
-  
-    // Sélectionner tous les éléments avec l'attribut de données, sauf ceux qui correspondent à l'attribut de données spécifié
+  }); 
     const autresElements = document.querySelectorAll(`[data-type]:not([data-type="${dataAttr}"])`);
-  
-    // Supprimer la classe "block" de chaque élément qui ne correspond pas à l'attribut de données spécifié
     autresElements.forEach(element => {
       element.classList.remove('block');
     });
-  }  
+  }    
   
-  // Sélectionner tous les boutons de catégorie
-  const buttonSlider = document.querySelectorAll('.button-slider');
+  const buttonSlider = document.querySelectorAll('.button-slider');  
   
-  // Ajouter un écouteur d'événement de clic à chaque bouton
   buttonSlider.forEach(button => {
-    button.addEventListener('click', () => {
-      // Récupérer la valeur de l'attribut de données du bouton
-      const type = button.getAttribute('data-type');
-  
-      // Afficher les éléments correspondant à la catégorie
+    button.addEventListener('click', () => {      
+      const type = button.getAttribute('data-type');      
       afficherElements(type);
     });
   });
